da1f5cf151db27dabbfbbaa5ef68b76c3e5587e9
Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>
2026-02-13 07:35:29 +0530

    RDKEMW-13563 : Integrate entservices-linearplaybackcontrol to middleware builds (#1)
    * RDKEMW-13563 : Integrate entservices-linearplaybackcontrol to middleware builds
    Reason For Change: Integrate entservices-linearplaybackcontrol to middleware builds
    Test procedure : Compile and Verify
    version: patch
    Priority: P2
    
    Signed-off-by: smanes0213 <sankalpmaneshwar46@outlook.com>
    
    * Resolve workfows
    
    * Resolve workfows
    
    * Resolve workfows
    
    * Resolve workflow issues
    
    * Resolve workflow issues
    
    * Resolve workflow issues
    
    * Clean service.cmake
    
    * Clean cov_build.sh
    
    * Clean up cov build scripts
    
    * Clean up cov build scripts
    
    * Resolve native build failure
    
    ---------
    
    Signed-off-by: smanes0213 <sankalpmaneshwar46@outlook.com>

--- cov_build.sh
+++ cov_build.sh
@@ -5,39 +5,35 @@ set -e
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
-# Build entservices-inputoutput
+# Build entservices-linearplaybackcontrol
 echo "======================================================================================"
-echo "buliding entservices-inputoutput"
+echo "buliding entservices-linearplaybackcontrol"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
+cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-linearplaybackcontrol \
 -DUSE_THUNDER_R4=ON \
 -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
 -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
 -DCMAKE_VERBOSE_MAKEFILE=ON \
--DCMAKE_DISABLE_FIND_PACKAGE_IARMBus=ON \
 -DCMAKE_DISABLE_FIND_PACKAGE_RFC=ON \
 -DCMAKE_DISABLE_FIND_PACKAGE_DS=ON \
 -DCOMCAST_CONFIG=OFF \
 -DRDK_SERVICES_COVERITY=ON \
 -DRDK_SERVICES_L1_TEST=ON \
 -DDS_FOUND=ON \
--DPLUGIN_SCREENCAPTURE=ON \
+-DPLUGIN_LINEARPLAYBACKCONTROL=ON \
 -DHAS_FRONT_PANEL=ON \
 -DHIDE_NON_EXTERNAL_SYMBOLS=OFF \
 -DCMAKE_CXX_FLAGS="-DEXCEPTIONS_ENABLE=ON \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/audiocapturemgr \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/rdk/ds \
--I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/rdk/iarmbus \
--I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/rdk/iarmmgrs-hal \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/ccec/drivers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/network \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/thunder \
 -I /usr/include/libdrm \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/devicesettings.h \
--include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Iarm.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Rfc.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/RBus.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/Telemetry.h \
@@ -51,12 +47,9 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/HdmiCec.h \
 -Wall -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
--DENABLE_TELEMETRY_LOGGING -DUSE_IARMBUS \
--DENABLE_SYSTEM_GET_STORE_DEMO_LINK -DENABLE_DEEP_SLEEP \
--DENABLE_SET_WAKEUP_SRC_CONFIG -DENABLE_THERMAL_PROTECTION \
--DUSE_DRM_SCREENCAPTURE -DHAS_API_SYSTEM -DHAS_API_POWERSTATE \
--DHAS_RBUS -DDISABLE_SECURITY_TOKEN -DENABLE_DEVICE_MANUFACTURER_INFO -DUSE_THUNDER_R4 -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
+-DENABLE_TELEMETRY_LOGGING -DHAS_API_SYSTEM \
+-DUSE_THUNDER_R4 -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
-cmake --build build/entservices-inputoutput --target install
+cmake --build build/entservices-linearplaybackcontrol --target install
 echo "======================================================================================"
 exit 0
